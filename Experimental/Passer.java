9145fc20f345b7bf75befd8295be1bdfd676a8c7
raunakchowdhury <rchowdhury5@stuy.edu>
2018-01-30 16:45:22 -0500

    accomodated for two semesters of CS

--- Experimental/Passer.java
+++ Experimental/Passer.java
@@ -1,37 +1,50 @@
 public class Passer {
 
+public static int _jerry;
+
+public Passer(){
+        System.out.println(_jerry + 1);
+}
 
 // change the val of  int a
 public static int addOne( int a){
+        int b;
+        //System.out.println(b+1);
         return a += 1;
 }
 
 public static String turnHey (){
         return "hey";
+
 }
 
 public static void main(String[] args) {
         //test of integers
-        System.out.println("========TESTING INTEGERS=========");
-        int a = 3;
-        System.out.println(a);
-        a = addOne(a);
-        System.out.println(a);
-        int b = 4;
-        //System.out.println(a == b);
-        // Conclusion: pass-by-ref
-        // a points to 4 instead of 3
+        Passer bert = new Passer();
+        System.out.println(_jerry);
+        /**
 
-        // ======NON PRIMITIVES============
-        System.out.println("===========NON-PRIMITIVES=========");
-        String c = "hey";
-        String f = "foo";
-        System.out.println(f.equals(c)); //false
-        f = turnHey();
-        System.out.println(c.equals(f)); //true
-        //Conclusion: Java is pass-by-ref. We set the String f to "hey", which caused the addresses of c and f to be equal.
+           System.out.println("========TESTING INTEGERS=========");
+           int a = 3;
+           System.out.println(a);
+           a = bert.addOne(a);
+           System.out.println(a);
+           int b = 4;
+           //System.out.println(a == b);
+           // Conclusion: pass-by-ref
+           // a points to 4 instead of 3
 
-        //String b = "ryan";
+           // ======NON PRIMITIVES============
+           System.out.println("===========NON-PRIMITIVES=========");
+           String c = "hey";
+           String f = "foo";
+           System.out.println(f.equals(c)); //false
+           f = turnHey();
+           System.out.println(c.equals(f)); //true
+           //Conclusion: Java is pass-by-ref. We set the String f to "hey", which caused the addresses of c and f to be equal.
+
+           //String b = "ryan";
+         **/
 
 }
 }
---
+++ Experimental/Tester.java
@@ -0,0 +1,9 @@
+public class Tester {
+public static int murk;
+public void meep(){
+        murk=0;
+
+};
+public void boop(){
+};
+}
