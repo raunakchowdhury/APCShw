32966ef6062b6abe79446f537188075badb1e1d6
raunakchowdhury <rchowdhury5@stuy.edu>
2018-03-02 09:00:13 -0500

    finsihed hw12

---
+++ HW12/Wat.java
@@ -0,0 +1,9 @@
+public class Wat {
+public static void main(String[] args) {
+        Boardasaurus bob = new Boardasaurus(15);
+        System.out.println(bob);
+        bob.go(15,2);
+
+        //Demo.Boardasaurus ben = new Demo.Boardasaurus(4);
+}
+}
---
+++ HW12/go.kt
@@ -0,0 +1,14 @@
+Raunak Chowdhury
+APCS2 pd2
+HW12 —-  __+3R|\/|1|\|4|_  1|_|_|\|3$$__
+2018-03-01
+
+0. It's interesting that you can put multiple class definitions in the same file, but what is the purpose? To define a subset class that is used in Demo?
+1. The main method checks to see if there were any arguments given by the user at the start. If there aren't any, the method catches errors and uses the default values instead.
+2. The program reverts to the default values.
+3. The recursion acts as a loop — it advances the "move" counter with every successful iteration. It is necessary because "go" is responsible for updating the screen every iteration.
+4. I expect to see different positions in the array take on different numbers.
+5. That would be dependent on the arguments given at runtime. My estimate is about 10 seconds.
+6. ANSI is a general term for referring to the codes that control the cursor location and other options in text terminals.
+7. The program took about three times longer than I expected. This is due to the delay; when I inputted 100 as the delay time (as opposed to 500) the program was far faster.
+8. Each number would denote the number of moves that the knight has made from the beginning, and show the progression of the algorithm to the user (as opposed to just generating a solved board).
