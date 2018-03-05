4453626bdc9e25a82a19e8334de45d34f2b6a4be
raunakchowdhury <rchowdhury5@stuy.edu>
2018-03-05 09:10:08 -0500

    modded hw12

--- HW12/Wat.java
+++ HW12/Wat.java
@@ -1,8 +1,9 @@
 public class Wat {
 public static void main(String[] args) {
         Boardasaurus bob = new Boardasaurus(15);
-        System.out.println(bob);
-        bob.go(15,2);
+        //System.out.println(bob);
+        //bob.go(15,2);
+        System.out.println("[2J");
 
         //Demo.Boardasaurus ben = new Demo.Boardasaurus(4);
 }
