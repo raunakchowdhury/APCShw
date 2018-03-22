31228e6cbac384df9e9b0a59af4ce8b917b2d8c5
raunakchowdhury <rchowdhury5@stuy.edu>
2018-03-22 10:32:16 -0400

    finished hw23

--- HW23/LLNode.java
+++ HW23/LLNode.java
@@ -1,78 +1,83 @@
 /*****************************************************
- * class LLNode
- * Implements a node, for use in lists and other container classes.
- * Stores its data as a String
- *****************************************************/
+* class LLNode
+* Implements a node, for use in lists and other container classes.
+* Stores its data as a String
+*****************************************************/
 
 public class LLNode
 {
-    //instance vars
-    private String _cargo;    //cargo may only be of type String
-    private LLNode _nextNode; //pointer to next LLNode
+//instance vars
+private String _cargo;        //cargo may only be of type String
+private LLNode _nextNode;     //pointer to next LLNode
 
-    // constructor -- initializes instance vars
-    public LLNode( String value, LLNode next ) {
-	_cargo = value;
-	_nextNode = next;
-    }
-
-
-    //--------------v  ACCESSORS  v--------------
-    public String getCargo() { return _cargo; }
-
-    public LLNode getNext() { return _nextNode; }
-    //--------------^  ACCESSORS  ^--------------
+// constructor -- initializes instance vars
+public LLNode( String value, LLNode next ) {
+        _cargo = value;
+        _nextNode = next;
+}
 
 
-    //--------------v  MUTATORS  v--------------
-    public String setCargo( String newCargo ) {
-	String foo = getCargo();
-	_cargo = newCargo;
-	return foo;
-    }
+//--------------v  ACCESSORS  v--------------
+public String getCargo() {
+        return _cargo;
+}
 
-    public LLNode setNext( LLNode newNext ) {
-	LLNode foo = getNext();
-	_nextNode = newNext;
-	return foo;
-    }
-    //--------------^  MUTATORS  ^--------------
-
-    
-    // override inherited toString
-    public String toString() { return _cargo.toString(); }
+public LLNode getNext() {
+        return _nextNode;
+}
+//--------------^  ACCESSORS  ^--------------
 
 
-    //main method for testing
-    public static void main( String[] args )
-    {
+//--------------v  MUTATORS  v--------------
+public String setCargo( String newCargo ) {
+        String foo = getCargo();
+        _cargo = newCargo;
+        return foo;
+}
 
-	//Below is an exercise in creating a linked list...
+public LLNode setNext( LLNode newNext ) {
+        LLNode foo = getNext();
+        _nextNode = newNext;
+        return foo;
+}
+//--------------^  MUTATORS  ^--------------
 
-	//Create a node
-	LLNode first = new LLNode( "cat", null );
 
-	//Create a new node after the first
-	first.setNext( new LLNode( "dog", null ) );
+// override inherited toString
+public String toString() {
+        return _cargo.toString();
+}
 
-	//Create a third node after the second
-	first.getNext().setNext( new LLNode( "cow", null ) );
 
-	/* A naive list traversal, has side effects.... ??
-	   while( first != null ) {
-	   System.out.println( first );
-	   first = first.getNext();
-	   }
-	*/
+//main method for testing
+public static void main( String[] args )
+{
 
-	//Q: when head ptr moves to next node in list, what happens to the node it just left?
+        //Below is an exercise in creating a linked list...
 
-	//...so better: ?
-	//
-	//
-	//
-	
-    }//end main
+        //Create a node
+        LLNode first = new LLNode( "cat", null );
+
+        //Create a new node after the first
+        first.setNext( new LLNode( "dog", null ) );
+
+        //Create a third node after the second
+        first.getNext().setNext( new LLNode( "cow", null ) );
+
+        /* A naive list traversal, has side effects.... ??
+           while( first != null ) {
+           System.out.println( first );
+           first = first.getNext();
+           }
+         */
+
+        //Q: when head ptr moves to next node in list, what happens to the node it just left?
+
+        //...so better: ?
+        //
+        //
+        //
+
+}    //end main
 
 }//end class LLNode
-
--- HW23/LList.java
+++ HW23/LList.java
@@ -1,126 +1,172 @@
 /*****************************************************
- * class LList
- * Implements a linked list of LLNodes, each containing String data
- *****************************************************/
+* class LList
+* Implements a linked list of LLNodes, each containing String data
+*****************************************************/
 
 public class LList implements List //your List interface must be in same dir
-{ 
+{
 
-    //instance vars
-    private LLNode _head;
-    private int _size;
+//instance vars
+private LLNode _head;
+private int _size;
 
-    // constructor -- initializes instance vars
-    public LList( )
-    {
-	_head = null; //at birth, a list has no elements
-	_size = 0;
-    }
-
-
-    //--------------v  List interface methods  v--------------
-    
-    public boolean add( String newVal )
-    {
-	LLNode tmp = new LLNode( newVal, _head );
-	_head = tmp;
-	_size++;
-	return true;
-    }
-
-    
-    public String get( int index )
-    {
-	if ( index < 0 || index >= size() )
-	    throw new IndexOutOfBoundsException();
-
-	String retVal;
-	LLNode tmp = _head; //create alias to head
-
-	//walk to desired node
-	for( int i=0; i < index; i++ )
-	    tmp = tmp.getNext();
-
-	//check target node's cargo hold
-	retVal = tmp.getCargo();
-	return retVal;
-    }
+// constructor -- initializes instance vars
+public LList( )
+{
+        _head = null; //at birth, a list has no elements
+        _size = 0;
+}
 
 
-    public String set( int index, String newVal )
-    {
+//--------------v  List interface methods  v--------------
 
-	if ( index < 0 || index >= size() )
-	    throw new IndexOutOfBoundsException();
-
-	LLNode tmp = _head; //create alias to head
-
-	//walk to desired node
-	for( int i=0; i < index; i++ )
-	    tmp = tmp.getNext();
-
-	//store target node's cargo
-	String oldVal = tmp.getCargo();
-
-	//modify target node's cargo
-	tmp.setCargo( newVal );
-
-	return oldVal;
-    }
+public boolean add( String newVal )
+{
+        LLNode tmp = new LLNode( newVal, _head );
+        _head = tmp;
+        _size++;
+        return true;
+}
 
 
-    //return number of nodes in list
-    public int size() { return _size; }
-    
-    //--------------^  List interface methods  ^--------------
+public String get( int index )
+{
+        if ( index < 0 || index >= size() )
+                throw new IndexOutOfBoundsException();
+
+        String retVal;
+        LLNode tmp = _head; //create alias to head
+
+        //walk to desired node
+        for( int i=0; i < index; i++ )
+                tmp = tmp.getNext();
+
+        //check target node's cargo hold
+        retVal = tmp.getCargo();
+        return retVal;
+}
 
 
-    // override inherited toString
-    public String toString()
-    {
-	String retStr = "HEAD->";
-	LLNode tmp = _head; //init tr
-	while( tmp != null ) {
-	    retStr += tmp.getCargo() + "->";
-	    tmp = tmp.getNext();
-	}
-	retStr += "NULL";
-	return retStr;
-    }
+public String set( int index, String newVal )
+{
+
+        if ( index < 0 || index >= size() )
+                throw new IndexOutOfBoundsException();
+
+        LLNode tmp = _head; //create alias to head
+
+        //walk to desired node
+        for( int i=0; i < index; i++ )
+                tmp = tmp.getNext();
+
+        //store target node's cargo
+        String oldVal = tmp.getCargo();
+
+        //modify target node's cargo
+        tmp.setCargo( newVal );
+
+        return oldVal;
+}
 
 
-    //main method for testing
-    public static void main( String[] args )
-    {
-	LList james = new LList();
+//return number of nodes in list
+public int size() {
+        return _size;
+}
 
-	System.out.println( james );
-	System.out.println( "size: " + james.size() );
+//--------------^  List interface methods  ^--------------
 
-	james.add("beat");
-	System.out.println( james );
-	System.out.println( "size: " + james.size() );
 
-	james.add("a");
-	System.out.println( james );
-	System.out.println( "size: " + james.size() );
+// override inherited toString
+public String toString()
+{
+        String retStr = "HEAD->";
+        LLNode tmp = _head; //init tr
+        while( tmp != null ) {
+                retStr += tmp.getCargo() + "->";
+                tmp = tmp.getNext();
+        }
+        retStr += "NULL";
+        return retStr;
+}
 
-	james.add("need");
-	System.out.println( james );
-	System.out.println( "size: " + james.size() );
+public String remove(int i){
+        LLNode first = _head;
+        LLNode deletedNode;
+        for (int node = 0; node <= i-1; node++)
+                first = first.getNext();
+        deletedNode = first.getNext();
+        first.setNext( deletedNode.getNext() ); //link the node before to the node after the "deleted" one
+        return deletedNode.getCargo();
+}
 
-	james.add("I");
-	System.out.println( james );
-	System.out.println( "size: " + james.size() );
+public void add( int i, String s ){
+        LLNode first = _head;
+        System.out.println(size());
+        // extreme conds
+        if (i == 0) {
+                add(s);
+                return;
+        }
+        if (i > size())
+                throw new IndexOutOfBoundsException();
+        // loop to cycle to the elemetn prior to where it should be added
+        for (int node = 0; node <= i-1; node++)
+                if (first.getNext() != null)
+                        first = first.getNext();
+        LLNode newNode = new LLNode (s,first.getNext()); //link it to the formerly next element
+        first.setNext(newNode); //link first to the added node
+}
 
-	System.out.println( "2nd item is: " + james.get(1) );
 
-	james.set( 1, "got" );
-	System.out.println( "...and now 2nd item is: " + james.set(1,"got") );
+//main method for testing
+public static void main( String[] args )
+{
+        LList james = new LList();
 
-	System.out.println( james );
-    }
+        System.out.println( james );
+        System.out.println( "size: " + james.size() );
+
+        james.add("beat");
+        System.out.println( james );
+        System.out.println( "size: " + james.size() );
+
+        james.add("a");
+        System.out.println( james );
+        System.out.println( "size: " + james.size() );
+
+        james.add("need");
+        System.out.println( james );
+        System.out.println( "size: " + james.size() );
+
+        james.add("I");
+        System.out.println( james );
+        System.out.println( "size: " + james.size() );
+
+        System.out.println( "2nd item is: " + james.get(1) );
+
+        james.set( 1, "got" );
+        System.out.println( "...and now 2nd item is: " + james.set(1,"got") );
+
+        System.out.println( james );
+
+        System.out.println();
+        System.out.println("Trying remove...");
+        System.out.println("Removing " + james.remove(1));
+        System.out.println(james);
+
+        System.out.println();
+        System.out.println("Trying add at index...");
+        System.out.println("Adding a to its former position...");
+        james.add(1,"a");
+        System.out.println(james);
+        System.out.println("Adding | to the ends of the array...");
+        james.add(0,"|");
+        System.out.println(james);
+        james.add(james.size(),"|");
+        System.out.println(james);
+        System.out.println("Trying to add outside of the array...");
+        james.add(90000,"rip");
+}
 
 }//end class LList
-
-
